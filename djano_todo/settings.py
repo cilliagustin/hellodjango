960d7c2e4aa7ec1c2c35ca204bd356cda22bdd8a
Agustin cilli <cilli.agustin@gmail.com>
2022-12-09 15:28:40 +0000

    a

--- djano_todo/settings.py
+++ djano_todo/settings.py
@@ -1,3 +1,10 @@
+from pathlib import Path
+import dj_database_url
+import os
+
+if os.path.isfile("env.py"):
+    import env
+
 """
 Django settings for djano_todo project.
 
@@ -74,11 +81,9 @@ WSGI_APPLICATION = 'djano_todo.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
 }
 
 
